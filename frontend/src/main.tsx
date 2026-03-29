7f52b187b33b2aebd0957859d0d9d85480110f61
Arden Vale <ardenvale13@gmail.com>
2026-03-29 21:01:25 +1300

    fhh

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,13 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
+import AuthGate from './components/AuthGate'
 import './styles/global.css'
 import { initPushNotifications } from './services/push'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <AuthGate>
+        <App />
+      </AuthGate>
     </BrowserRouter>
   </React.StrictMode>,
 )
